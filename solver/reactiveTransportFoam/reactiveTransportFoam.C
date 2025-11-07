3597337e452b570d391f6d0720a7b75107df89df
Matteo Icardi <mat.icardi@gmail.com>
2025-11-07 15:09:27 +0000

    updated structure and headers

--- solver/reactiveTransportFoam/reactiveTransportFoam.C
+++ solver/reactiveTransportFoam/reactiveTransportFoam.C
@@ -1,33 +1,35 @@
 /*---------------------------------------------------------------------------*\
-=========                 |
-\\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
- \\    /   O peration     | Website:  https://openfoam.org
-  \\  /    A nd           | Copyright (C) 2011-2018 OpenFOAM Foundation
-   \\/     M anipulation  |
+  =========                 |
+  \\      /  F ield         | OpenFOAM: The Open Source CFD Toolbox
+   \\    /   O peration     |
+    \\  /    A nd           | Copyright (C) 2011-2020 The OpenFOAM Foundation
+     \\/     M anipulation  |
+-------------------------------------------------------------------------------
+                      Copyright (C) 2025 Matteo Icardi, Diego Fida
 -------------------------------------------------------------------------------
 License
-This file is part of OpenFOAM.
+    This file is part of scalarAPRFoam.
 
-OpenFOAM is free software: you can redistribute it and/or modify it
-under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    scalarAPRFoam is free software: you can redistribute it and/or modify it
+    under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-OpenFOAM is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License
-for more details.
+    scalarAPRFoam is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with OpenFOAM.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with scalarAPRFoam.  If not, see <http://www.gnu.org/licenses/>.
 
 Application
-reactiveTransportFoam
+    reactiveTransportFoam
 
 Description
-Transient solver for multiple species transport with
-arbitrary volumetric reaction
-Coupling is implemented using a segregated algorithm.
+    Transient solver for multiple species transport with
+    arbitrary volumetric reaction
+    Coupling is implemented using a segregated algorithm.
 
 \*---------------------------------------------------------------------------*/
 
